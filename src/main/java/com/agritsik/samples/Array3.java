0df61a23608017781d0f05ba149cb4fd31fd0af7
andrey <a.gritsik@gmail.com>
2015-09-30 17:47:15 +0300

    add more

--- src/main/java/com/agritsik/samples/Array3.java
+++ src/main/java/com/agritsik/samples/Array3.java
@@ -5,6 +5,38 @@ package com.agritsik.samples;
  */
 public class Array3 {
 
+
+    /*
+        Consider the leftmost and righmost appearances of some value in an array.
+        We'll say that the "span" is the number of elements between the two inclusive.
+        A single value has a span of 1. Returns the largest span found in the given array.
+        (Efficiency is not a priority.)
+
+        maxSpan({1, 2, 1, 1, 3}) → 4
+        maxSpan({1, 4, 2, 1, 4, 1, 4}) → 6
+        maxSpan({1, 4, 2, 1, 4, 4, 4}) → 6
+    */
+    public int maxSpan(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int interval = 1;
+        int i = 0;
+        while (i < nums.length) {
+
+            int j = nums.length - 1;
+            while (j >= i) {
+                if (nums[i] == nums[j] && j - i + 1 > interval) {
+                    interval = j - i + 1;
+                }
+                j--;
+            }
+            i++;
+        }
+
+        return interval;
+    }
+
+
     /*
         Given a non-empty array, return true if there is a place
         to split the array so that the sum of the numbers on one side is equal
