d0cdbe960c38b10e252308a3e476a8dc5ce3aa4f
andrey <a.gritsik@gmail.com>
2015-09-30 22:08:31 +0300

    add more

--- src/main/java/com/agritsik/samples/Array2.java
+++ src/main/java/com/agritsik/samples/Array2.java
@@ -33,4 +33,24 @@ public class Array2 {
         return sum / ints.length;
     }
 
+    public int sum67(int[] nums) {
+
+        int sum = 0;
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == 6) {
+                while (nums[i] != 7) {
+                    i++;
+                }
+            } else {
+                sum += nums[i];
+            }
+
+            i++;
+        }
+
+        return sum;
+    }
 }
+
+
