9521366b6dfe9a5df7609fc64b021fc9ef5503d8
andrey <a.gritsik@gmail.com>
2015-09-30 17:55:55 +0300

    add more

---
+++ src/main/java/com/agritsik/samples/String3.java
@@ -0,0 +1,26 @@
+package com.agritsik.samples;
+
+/**
+ * Created by andrey on 9/30/15.
+ */
+public class String3 {
+
+    public int countYZ(String str) {
+        char[] chars = str.toCharArray();
+        int count = 0;
+
+        int i = 0;
+        while (i < chars.length) {
+            if ((chars[i] == 'y' || chars[i] == 'Y' || chars[i] == 'z' || chars[i] == 'Z')
+                    && (i + 1 == chars.length || !Character.isLetter(chars[i + 1])))
+                count++;
+            i++;
+        }
+
+        return count;
+    }
+
+
+
+
+}
